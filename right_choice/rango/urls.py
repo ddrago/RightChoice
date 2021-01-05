66e743a728f9f5f9cd944888542f5bc9db66dcee
2389677w <2389677W@student.gla.ac.uk>
2021-01-05 15:21:40 +0000

    added url and views for all non slug pages

--- right_choice/rango/urls.py
+++ right_choice/rango/urls.py
@@ -3,6 +3,9 @@ from rango import views
 app_name = 'rango'
 urlpatterns = [
 path('', views.index, name='index'),
-path('about/', views.about, name='about')
+path('about/', views.about, name='about'),
+path('university/', views.uni, name='university'),
+path('college/', views.college, name='college'),
+path('apprenticeship/', views.apprenticeship, name='apprenticeship'),
 ]
 
--- right_choice/rango/views.py
+++ right_choice/rango/views.py
@@ -8,3 +8,15 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'Created by Stuart, Euan, Diego, Zhenkun and Daniyal'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def uni(request):
+    context_dict = {'boldmessage': 'Look at all the couses avalible or search for a desired course'}
+    return render(request, 'rango/university.html', context=context_dict)
+
+def college(request):
+    context_dict = {'boldmessage': 'Look at all the couses avalible or search for a desired course'}
+    return render(request, 'rango/college.html', context=context_dict)
+
+def apprenticeship(request):
+    context_dict = {'boldmessage': 'Look at all the companies offering apprenticeships'}
+    return render(request, 'rango/apprenticeship.html', context=context_dict)
