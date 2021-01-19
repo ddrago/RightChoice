bef5957f1b799450fe8451b5f4c3462abb631ff0
Daniyal Khan <2386193k@student.gla.ac.uk>
2021-01-19 16:16:40 +0000

    Homepage wihtout functionality

--- right_choice/rango/views.py
+++ right_choice/rango/views.py
@@ -10,13 +10,14 @@ def about(request):
     return render(request, 'rango/about.html', context=context_dict)
 
 def uni(request):
-    context_dict = {'boldmessage': 'Look at all the couses avalible or search for a desired course'}
+    context_dict = {'boldmessage': 'Look at all the courses available or search for a desired course'}
     return render(request, 'rango/university.html', context=context_dict)
 
 def college(request):
-    context_dict = {'boldmessage': 'Look at all the couses avalible or search for a desired course'}
+    context_dict = {'boldmessage': 'Look at all the courses available or search for a desired course'}
     return render(request, 'rango/college.html', context=context_dict)
 
 def apprenticeship(request):
     context_dict = {'boldmessage': 'Look at all the companies offering apprenticeships'}
     return render(request, 'rango/apprenticeship.html', context=context_dict)
+
