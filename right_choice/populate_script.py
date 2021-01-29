337e6d34b4589a4db764af7461da3263ef3103d6
Euan Wills <2389677w@student.gla.ac.uk>
2021-01-29 17:18:25 +0000

    fixed some population script issues and started course pages

--- right_choice/populate_script.py
+++ right_choice/populate_script.py
@@ -17,7 +17,8 @@ def populate():
     for c in Career.objects.all():
         print(f'-{c}')
 
-    school_subjects_computing_higher = [{'name': 'Computing Science', 'level': 'Higher', 'career_name':"Software Engineer", 'subjectArea': 'Computing'},{'name': 'Mathematics', 'level': 'Higher', 'career_name':"Mathematician", 'subjectArea': 'Maths'}]
+    school_subjects_computing_higher = [{'name': 'Computing Science', 'level': 'Higher', 'career_name':"Software Engineer",
+                                        'subjectArea': 'Computing'},{'name': 'Mathematics', 'level': 'Higher', 'career_name':"Mathematician", 'subjectArea': 'Maths'}]
     #school_subjects_business_higher = [{'name': 'Business Studies', 'level': 'Higher', 'career_name':finance_career, 'subjectArea': 'Business'}, {'name': 'Mathamatics', 'level': 'Higher', 'career_name':finance_career, 'subjectArea': 'Maths'}]
     for d in school_subjects_computing_higher:
         t = SchoolSubjects.objects.get_or_create(name=d["name"], level=d["level"], subjectArea=d["subjectArea"])[0]
@@ -27,11 +28,12 @@ def populate():
         print(f'-{c}')
 
 
-    school_subjects_computing_nat5 = [{'name': 'Compuitng Science', 'level': 'Nat 5', 'career_name':"Software Engineer", 'subjectArea': 'Computing'},{'name': 'Mathematics', 'level': 'Nat 5', 'career_name':"Mathematician", 'subjectArea': 'Maths'}]
+    school_subjects_computing_nat5 = [{'name': 'Compuitng Science', 'level': 'Nat 5', 'career_name':"Software Engineer", 'subjectArea': 'Computing'},
+                                      {'name': 'Mathematics', 'level': 'Nat 5', 'career_name':"Mathematician", 'subjectArea': 'Maths'}]
     #school_subjects_business_nat5 = [{'name': 'Business Studies', 'level': 'Nat 5', 'career_name':finance_career, 'subjectArea': 'Business'}, {'name': 'Mathamatics', 'level': 'Higher', 'career_name':finance_career, 'subjectArea': 'Maths'}]
     for d in school_subjects_computing_nat5:
         t = SchoolSubjects.objects.get_or_create(name=d["name"],level=d["level"], subjectArea=d["subjectArea"])[0]
-        t.career_name.filter(careerName=d["career_name"])
+        t.career_name.add(d["career_name"])
         t.save()
     for c in SchoolSubjects.objects.all():
         print(f'-{c}')
@@ -39,54 +41,58 @@ def populate():
 
     Colleges = [{'courses': None, 'name': 'City of Glasgow College', 'location':'Glasgow', 'details': 'The city of glasgow college is located right in the center of glasgow.', 'collegeImage':'city_of_glasgow_college.png', 'linkToCollegeWebsite':'https://www.cityofglasgowcollege.ac.uk/'}]
     for d in Colleges:
-        t = College.objects.get_or_create(name=d["name"], location=d["location"], details=d["details"], linkToCollegeWebsite=d["linkToCollegeWebsite"])[0]
+        t = College.objects.get_or_create(name=d["name"], location=d["location"], details=d["details"], collegeImage=d["collegeImage"], linkToCollegeWebsite=d["linkToCollegeWebsite"])[0]
         t.save()
     for c in College.objects.all():
         print(f'-{c}')
 
 
-    courses_city_of_glasgow_college = [{'courseID':1, 'name':'Computing Science HNC', 'collegeName': College.objects.get(name="City of Glasgow College"), 'career_name':Career.objects.get(areaStudy="Computing Science"), 'subReqName':SchoolSubjects.objects.get(name="Mathematics", level="Nat 5"), 'gradesReq': 'CC', 'subjectSuggestions':'Computing Science, Maths and Physics'},
-                                    {'courseID': 2, 'name': 'Finance and Accounting HNC', 'collegeName': College.objects.get(name="City of Glasgow College"), 'career_name': Career.objects.get(areaStudy="Mathematician"), 'subReqName':SchoolSubjects.objects.get(name="Mathematics", level="Nat 5"), 'gradesReq': 'CC', 'subjectSuggestions':'Business, Maths and Economics'}]
+    courses_city_of_glasgow_college = [{'courseID':1, 'name':'Computing Science HNC', 'collegeName': College.objects.get(name="City of Glasgow College"), 'career_name':Career.objects.filter(areaStudy="Computing Science"), 'subReqName':SchoolSubjects.objects.filter(name="Mathematics", level="Nat 5"), 'gradesReq': 'CC', 'subjectSuggestions':'Computing Science, Maths and Physics'},
+                                    {'courseID': 2, 'name': 'Finance and Accounting HNC', 'collegeName': College.objects.get(name="City of Glasgow College"), 'career_name': Career.objects.filter(areaStudy="Mathematician"), 'subReqName':SchoolSubjects.objects.filter(name="Mathematics", level="Nat 5"), 'gradesReq': 'CC', 'subjectSuggestions':'Business, Maths and Economics'}]
     for d in courses_city_of_glasgow_college:
         t = Course_College.objects.get_or_create(courseID=d["courseID"], name=d["name"], collegeName=d["collegeName"], gradesReq=d["gradesReq"], subjectSuggestions=d["subjectSuggestions"])[0]
-        t.careerName.filter(careerName=d["career_name"])
-        t.subReqName.filter(name=d["subReqName"])
+        t.careerName.set(d["career_name"])
+        t.subReqName.set(d["subReqName"])
         t.save()
     for c in Course_College.objects.all():
         print(f'-{c}')
 
     Universities = [{'courses': None, 'name': 'Glasgow University', 'location': 'Glasgow', 'details': 'One of Scotlands finest universites and the fourth oldest uni in the world', 'universityImage':'Glasgow_University.jpg', 'linkToUniWebsite': 'https://www.gla.ac.uk/'}]
     for d in Universities:
-        t = University.objects.get_or_create(name=d['name'], location=d['location'], details=d['details'])[0]
+        t = University.objects.get_or_create(name=d['name'], location=d['location'], details=d['details'], universityImage=d["universityImage"], linkToUniWebsite=d["linkToUniWebsite"])[0]
         t.save()
 
     for c in University.objects.all():
         print(f'-{c}')
 
 
-    courses_glasgow_uni = [{'courseID': 1, 'name': 'Computing Science Bsc', 'universityName':University.objects.get(name="Glasgow University"), 'career_name': Career.objects.get(areaStudy="Computing Science"), 'subReqName':SchoolSubjects.objects.get(name="Mathematics", level="Higher"), 'gradesReq': 'AAAB', 'subjectSuggestions':'Computing Science, Maths and Physics'},
-                           {'courseID': 2, 'name': 'Finance and Accounting Bsc', 'universityName':University.objects.get(name="Glasgow University"), 'career_name': Career.objects.get(areaStudy="Mathematician"), 'subReqName':SchoolSubjects.objects.get(name="Mathematics", level="Higher"), 'gradesReq': 'AAAB', 'subjectSuggestions':'Business, Maths and Economics'}]
+    courses_glasgow_uni = [{'courseID': 1, 'name': 'Computing Science Bsc', 'universityName':University.objects.get(name="Glasgow University"), 'career_name': Career.objects.filter(areaStudy="Computing Science"), 'subReqName':SchoolSubjects.objects.filter(name="Mathematics", level="Higher"), 'gradesReq': 'AAAB', 'subjectSuggestions':'Computing Science, Maths and Physics'},
+                           {'courseID': 2, 'name': 'Finance and Accounting Bsc', 'universityName':University.objects.get(name="Glasgow University"), 'career_name': Career.objects.filter(areaStudy="Mathematician"), 'subReqName':SchoolSubjects.objects.filter(name="Mathematics", level="Higher"), 'gradesReq': 'AAAB', 'subjectSuggestions':'Business, Maths and Economics'}]
+
+    print(Career.objects.filter(areaStudy="Mathematician"))
     for d in courses_glasgow_uni:
         t = Course_Uni.objects.get_or_create(courseID=d["courseID"], name=d["name"], universityName=d["universityName"], gradesReq=d["gradesReq"], subjectSuggestions=d["subjectSuggestions"])[0]
-        t.careerName.filter(careerName=d["career_name"])
-        t.subReqName.filter(name=d["subReqName"])
+        t.careerName.set(d["career_name"])
+        print(d["career_name"])
+        t.subReqName.set(d["subReqName"])
         t.save()
     for c in Course_Uni.objects.all():
         print(f'-{c}')
 
-    company = [{'name': "Apprenticeship Scotland", 'location': "edinburgh", "details": "The leading provider of apprentieships in Scotland.", 'linkToCompanyWebsite': "jjjj" }]
+    company = [{'name': "Apprenticeship Scotland", 'location': "edinburgh", "details": "The leading provider of apprentieships in Scotland.", 'linkToCompanyWebsite': "jjjj", 'companyImage':'apprenticeship.png' }]
 
     for d in company:
-        t = Company.objects.get_or_create(name=d["name"], location=d["location"], details=d["details"], linkToCompanyWebsite=d["linkToCompanyWebsite"])[0]
+        t = Company.objects.get_or_create(name=d["name"], location=d["location"], details=d["details"], linkToCompanyWebsite=d["linkToCompanyWebsite"], companyImage=d["companyImage"])[0]
         t.save()
     for c in Company.objects.all():
         print(f'-{c}')
 
 
-    courses_apprenticeships_scotland = [{'courseID':1, 'name':'Accounting Apprenticeship', 'companyName': Company.objects.get(name='Apprenticeship Scotland'), 'careerName':Career.objects.get(areaStudy="Mathematician"), 'gradesReq': 'No qualifications required', 'subjectSuggestions':'Business, Maths and Economics'}]
+    courses_apprenticeships_scotland = [{'courseID':1, 'name':'Accounting Apprenticeship', 'companyName': Company.objects.get(name='Apprenticeship Scotland'), 'careerName':Career.objects.filter(areaStudy="Mathematician"), 'gradesReq': 'No qualifications required', 'subjectSuggestions':'Business, Maths and Economics'}]
     for d in courses_apprenticeships_scotland:
         t = Course_Apprenticeship.objects.get_or_create(courseID=d["courseID"], name=d["name"], companyName=d["companyName"], gradesReq=d["gradesReq"], subjectSuggestions=d["subjectSuggestions"])[0]
-        t.careerName.filter(careerName=d["careerName"])
+        t.careerName.set(d["careerName"])
+        print(d["careerName"])
         t.save()
     for c in Course_Apprenticeship.objects.all():
         print(f'-{c}')
@@ -100,6 +106,7 @@ def populate():
     for c in Apprenticeship.objects.all():
         print(f'-{c}')
 
+    print(Career.objects.get(areaStudy="Computing Science"))
   #  user = User(username='defaultUserName', email='default_user@gmail.com', password='defaultPassword123')
   #  user.save()
 
